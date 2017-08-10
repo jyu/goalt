f7b46315df54c403cc8884ce0b7223e6bdedfbd4
Jerry Yu <jerryyu8@gmail.com>
2017-08-09 23:25:30 -0700

    reverse order of logs

--- app.js
+++ app.js
@@ -689,6 +689,7 @@ function sendMotivation(senderID) {
 function viewLogs(senderID, goal, index) {
   var message = "Here are your logs for " + goal.name +
                 " (Page " + (index/5 + 1) + ")\u000A";
+  goal.log = goal.log.reverse()
   for (var i = index; i < Math.min(goal.log.length, index + 5); i++) {
       message += goal.log[i];
       message +=  "\u000A";
